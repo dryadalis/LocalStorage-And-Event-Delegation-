dbc3365d5654cacf79dff5c96871d5e0be1abced
Karolina <kika.romanska@gmail.com>
2018-01-16 10:59:14 +0100

    Sellect all button is working now!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) || []; 
+const items = JSON.parse(localStorage.getItem('items')) || [];
 const clearAllButton = document.querySelector('.clearAllButton');
 const checkAllButton = document.querySelector('.checkAllButton');
 
@@ -21,7 +21,7 @@ function addItem(e) {
 
 function populateList(plates = [], platesList) {
     platesList.innerHTML = plates.map((plate, i) => {
-      return `
+        return `
         <li>
         <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
           <label for="item${i}">${plate.text}</label>
@@ -33,18 +33,18 @@ function populateList(plates = [], platesList) {
         loading.innerHTML = "Loading Tapas... "
         loading.classList.add("loading")
         document.querySelector('.loading').appendChild(loading);
-       
-    }
-  }
 
-  function toggleDone(e) {
-      if (!e.target.matches('input')) return; // skip this unless it's an input
+    }
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
     const el = e.target;
     const index = el.dataset.index;
     items[index].done = !items[index].done;
     localStorage.setItem('items', JSON.stringify(items));
     populateList(items, itemsList)
-  }
+}
 
 function clearAll(e) {
     e.preventDefault();
@@ -55,6 +55,10 @@ function clearAll(e) {
 
 function checkAll(e) {
     e.preventDefault();
+    const inputs = document.querySelectorAll("input[type='checkbox']");
+    inputs.forEach(input => {
+        input.checked = true;
+    });
 }
 
 
@@ -63,5 +67,3 @@ itemsList.addEventListener('click', toggleDone);
 clearAllButton.addEventListener('click', clearAll);
 checkAllButton.addEventListener('click', checkAll);
 populateList(items, itemsList)
-
-
